468e965c71d9e88970f784e2294f0497fe06748c
Kranthi0003 <40112006+Kranthi0003@users.noreply.github.com>
2019-02-12 00:13:17 +0530

    first commit

---
+++ ML.java
@@ -0,0 +1,178 @@
+import java.awt.*;
+
+import java.applet.*;
+
+public class ML extends Applet
+
+{
+    
+	Font f1;       
+   	public void  init()    
+    	{    
+       	f1 = new Font("Arial",Font.PLAIN,80);      
+    	}	  
+	public void paint(Graphics ob)
+    
+	{
+        
+		Color clr1=new Color(0,25,50);
+   
+		setBackground(clr1);
+  
+		Color clr2=new Color(10,200,240);
+		ob.setColor(Color.white);
+		ob.drawLine(655,218,680,260);
+		ob.drawLine(590,240,680,280);
+		ob.drawLine(633,380,700,280);
+		ob.drawLine(587,245,593,280);
+		ob.drawLine(590,130,585,230);
+		ob.drawLine(635,195,690,140);
+		ob.drawLine(745,113,702,133);
+		ob.drawLine(700,140,722,180);
+		ob.drawLine(779,156,735,200);
+		ob.drawLine(800,200,745,204);
+		ob.drawLine(830,245,750,215);
+		ob.drawLine(812,210,775,264);
+		ob.drawLine(690,288,770,328);
+		ob.drawLine(732,380,772,336);
+		ob.drawLine(820,313,782,330);
+		ob.drawLine(840,313,825,365);
+		ob.drawLine(785,405,737,365);
+		ob.drawLine(800,410,740,500);
+		ob.drawLine(750,110,785,145);
+		ob.drawLine(707,425,750,365);
+		ob.drawLine(695,435,690,495);
+		ob.drawLine(665,499,685,500);
+		ob.drawLine(550,357,582,373);
+		ob.drawLine(545,348,580,312);
+		ob.drawLine(530,245,575,280);
+		ob.drawLine(635,200,655,100);
+		ob.drawLine(555,403,583,430);
+		ob.drawLine(640,500,690,440);
+		ob.drawLine(645,495,647,425);
+		ob.drawLine(690,370,647,420);
+		ob.drawLine(690,370,720,370);
+		ob.drawLine(690,370,650,379);
+		ob.drawLine(695,370,695,320);
+		ob.drawLine(740,470,750,440);
+		ob.drawLine(700,435,750,440);
+		ob.drawLine(780,420,750,440);
+		ob.drawLine(800,420,825,370);
+		ob.drawLine(750,375,830,365);
+		ob.drawLine(779,336,820,365);
+		ob.drawLine(838,290,833,250);
+		ob.drawLine(790,265,830,250);
+		ob.drawLine(696,130,696,100);
+		ob.drawLine(640,110,590,123);
+		ob.drawLine(535,210,581,234);
+		ob.drawLine(560,187,582,232);
+		ob.drawLine(522,292,532,260);
+		ob.drawLine(522,295,582,295);
+		ob.drawLine(592,375,640,371);
+		ob.setColor(clr2);
+		ob.drawOval(650,250,78,78);
+		ob.fillOval(650,250,78,78);
+		ob.setColor(clr2);
+		ob.drawOval(570,270,50,50);
+		ob.fillOval(570,270,50,50);
+		ob.setColor(clr2);
+		ob.drawOval(750,250,45,45);
+		ob.fillOval(750,250,45,45);
+		ob.setColor(clr2);
+		ob.drawOval(708,178,50,50);
+		ob.fillOval(708,178,50,50);
+		ob.setColor(clr2);
+		ob.drawOval(610,160,63,63);
+		ob.fillOval(610,160,63,63);
+		ob.setColor(clr2);
+		ob.setColor(Color.white);
+		int arrx1[]={722,721,705,704};
+		int arry1[]={228,227,253,252};
+		ob.drawPolygon(arrx1,arry1,4);
+		ob.fillPolygon(arrx1,arry1,4);
+		ob.setColor(clr2);
+		ob.drawOval(670,410,50,50);
+		ob.fillOval(670,410,50,50);
+		ob.setColor(clr2);
+		ob.drawOval(620,350,45,45);
+		ob.fillOval(620,350,45,45);
+		ob.setColor(clr2);
+		ob.drawOval(720,350,45,45);
+		ob.fillOval(720,350,45,45);
+		ob.drawOval(775,395,45,45);
+		ob.fillOval(775,395,45,45);
+		ob.drawOval(720,470,42,42);
+		ob.fillOval(720,470,42,42);
+		ob.drawOval(820,290,35,35);
+		ob.fillOval(820,290,35,35);
+		ob.drawOval(800,185,30,30);
+		ob.fillOval(800,185,30,30);	
+		ob.drawOval(725,100,40,40);
+		ob.fillOval(725,100,40,40);
+		ob.drawOval(635,90,35,35);
+		ob.fillOval(635,90,35,35);
+		ob.drawOval(530,145,45,45);
+		ob.fillOval(530,145,45,45);
+		ob.drawOval(515,230,40,40);
+		ob.fillOval(515,230,40,40);
+		ob.drawOval(525,340,25,25);
+		ob.fillOval(525,340,25,25);
+		ob.drawOval(570,410,50,50);
+		ob.fillOval(570,410,50,50);
+		ob.drawOval(630,480,35,35);
+		ob.fillOval(630,480,35,35);
+		ob.setColor(Color.white); 
+		ob.drawRoundRect(650,542,80,15,10,10);
+		ob.fillRoundRect(650,542,80,15,10,10);
+		ob.drawRoundRect(650,565,80,15,10,10);
+		ob.fillRoundRect(650,565,80,15,10,10);
+		ob.drawRoundRect(650,588,80,15,10,10);
+		ob.fillRoundRect(650,588,80,15,10,10);
+		ob.drawRoundRect(659,611,62,15,10,10);
+		ob.fillRoundRect(659,611,62,15,10,10);
+		ob.drawOval(685,495,12,12);
+		ob.drawOval(580,370,12,12);
+		ob.drawOval(580,230,12,12);
+		ob.drawOval(770,325,12,12);
+		ob.drawOval(690,130,12,12);
+		ob.drawOval(780,145,12,12);
+		//dot1
+		ob.drawOval(550,395,12,12);
+		ob.fillOval(550,395,12,12);
+		ob.drawLine(555,403,580,380);
+		//dot2
+		ob.drawOval(515,290,12,12);
+		ob.fillOval(515,290,12,12);
+		//dot3
+		ob.drawOval(522,200,12,12);
+		ob.fillOval(522,200,12,12);
+		//dot4
+		ob.drawOval(585,118,12,12);
+		ob.fillOval(585,118,12,12);
+		//dot5
+		ob.drawOval(690,87,12,12);
+		ob.fillOval(690,87,12,12);
+		//dot6
+		ob.drawOval(825,240,12,12);
+		ob.fillOval(825,240,12,12);
+		//dot7
+		ob.drawOval(820,360,12,12);
+		ob.fillOval(820,360,12,12);
+		//dot8
+		ob.drawOval(640,415,12,12);
+		ob.fillOval(640,415,12,12);
+		//dot9
+		ob.drawOval(688,363,12,12);
+		ob.fillOval(688,363,12,12);
+		//dot10
+		ob.drawOval(742,435,12,12);
+		ob.fillOval(742,435,12,12);		
+		ob.setFont(f1);     
+      		ob.drawString("MACHINE",78,330);
+		ob.drawString("LEARNING",68,430); 
+	
+	}
+
+}
+/*<applet code="ML" height=1000 width=1000>
+</applet>*/
